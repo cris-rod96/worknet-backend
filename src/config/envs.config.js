e72efd1bca2cc004ca4ae2d0ed7941e7d14a4256
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-10-24 03:24:15 -0500

    Database connection and model creation

--- src/config/envs.config.js
+++ src/config/envs.config.js
@@ -1,5 +1,26 @@
 import 'dotenv/config'
 
-const { PORT = 3000, NODE_ENV = 'development' } = process.env
+const { PORT = 3000, NODE_ENV = 'development', DB_DEV, DB_PROD } = process.env
 
-export { PORT, NODE_ENV }
+const CONNECTION = {
+  URI: NODE_ENV === 'production' ? DB_PROD : DB_DEV,
+  CONFIG:
+    NODE_ENV === 'production'
+      ? {
+          native: false,
+          logging: false,
+          dialect: 'postgres',
+          dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false,
+            },
+          },
+        }
+      : {
+          native: false,
+          logging: false,
+        },
+}
+
+export { PORT, NODE_ENV, CONNECTION }
